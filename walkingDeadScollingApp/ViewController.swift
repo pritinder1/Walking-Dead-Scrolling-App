337858b943737f0d63962030f55a0aeb39ffd2af
pritinder1 <Pritinder@Pritinders-MBP.home>
2016-07-07 11:02:56 -0400

    Very Simple Scrolling App

--- walkingDeadScollingApp/ViewController.swift
+++ walkingDeadScollingApp/ViewController.swift
@@ -10,15 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    let WIDTH: CGFloat = 259.0
+    let HEIGHT: CGFloat = 422.0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        
+        for var i = 1; i <= 5; i+=1 {
+        
+        
+            let img = UIImage(named: "\(i)")
+            let imgView = UIImageView(image: img)
+            
+            scrollView.addSubview(imgView)
+            
+            imgView.frame = CGRectMake( -WIDTH + (WIDTH * CGFloat(i)), 250, WIDTH, HEIGHT)
+        
+        }
+        
+        scrollView.contentSize = CGSizeMake(WIDTH * 5, scrollView.frame.size.height)
+        
+        
+        
+        
+            }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
